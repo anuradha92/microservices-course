35b350ac1be59b08ac0705ca23d635e0b2ad2f58
Anuradha_ObjectOne <Anuradha.Gunasinghe@dialog.lk>
2021-06-19 10:06:58 +0530

    create auth sever

--- auth-server/src/main/java/com/anuradha/rentcloud/auth/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/com/anuradha/rentcloud/auth/authserver/AuthServerApplication.java
@@ -2,11 +2,14 @@ package com.anuradha.rentcloud.auth.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AuthServerApplication.class, args);
 	}
 
